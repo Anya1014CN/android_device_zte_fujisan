cf923773ea6f4683daf0f721ec77197141cb6997
Anya1014CN <han.cf@foxmail.com>
2026-07-08 16:56:35 +0800

    fujisan: reduce dual-screen app to boot sync

--- duallcdservice/src/com/zte/duallcd/BootReceiver.java
+++ duallcdservice/src/com/zte/duallcd/BootReceiver.java
@@ -3,11 +3,19 @@ package com.zte.duallcd;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.zte.dualLcdManager.DisplayModeManager;
 
 public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = "DualLcdBootReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        DualLcdApplication.ensureInitialized(context);
-        DualLcdApplication.syncNow();
+        try {
+            DisplayModeManager.getInstance(context).refreshCurrentState();
+        } catch (Throwable t) {
+            Log.e(TAG, "Failed to refresh dual-screen state", t);
+        }
     }
 }
