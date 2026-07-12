07f9537488544c61e3c5404e6e1234a5811a28c8
Anya1014CN <han.cf@foxmail.com>
2026-07-12 21:20:39 +0800

    fujisan: stop overriding android 11 init stages

--- device.mk
+++ device.mk
@@ -19,19 +19,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.product.first_api_level=25
 
 PRODUCT_PACKAGES += \
-    init.qcom.rc \
     libdolbyshim
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.qcom.rc:$(TARGET_COPY_OUT_ROOT)/init.qcom.rc \
-    $(LOCAL_PATH)/rootdir/init.dualscreen.rc:$(TARGET_COPY_OUT_ROOT)/init.dualscreen.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/fujisan.rc \
     $(LOCAL_PATH)/rootdir/firmware/.placeholder:$(TARGET_COPY_OUT_RAMDISK)/firmware/.placeholder \
     $(LOCAL_PATH)/rootdir/bt_firmware/.placeholder:$(TARGET_COPY_OUT_RAMDISK)/bt_firmware/.placeholder \
     $(LOCAL_PATH)/rootdir/dsp/.placeholder:$(TARGET_COPY_OUT_RAMDISK)/dsp/.placeholder \
     $(LOCAL_PATH)/rootdir/persist/.placeholder:$(TARGET_COPY_OUT_RAMDISK)/persist/.placeholder \
-    $(LOCAL_PATH)/rootdir/init.qcom.rc:$(TARGET_COPY_OUT_RAMDISK)/init.qcom.rc \
-    $(LOCAL_PATH)/rootdir/init.dualscreen.rc:$(TARGET_COPY_OUT_RAMDISK)/init.dualscreen.rc \
     $(LOCAL_PATH)/rootdir/etc/fstab.ramdisk.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(LOCAL_PATH)/system/usr/idc/goodix-touchscreen.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/goodix-touchscreen.idc \
     $(LOCAL_PATH)/system/usr/idc/synaptics_dsx.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/synaptics_dsx.idc \
