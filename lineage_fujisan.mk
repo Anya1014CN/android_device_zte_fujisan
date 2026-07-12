216eb84f6232eafd10ab8e093adbf41f71cae928
Anya1014CN <han.cf@foxmail.com>
2026-07-12 15:35:56 +0800

    fujisan: restore stock ramdisk bring-up pieces

--- lineage_fujisan.mk
+++ lineage_fujisan.mk
@@ -1,5 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/verity.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/zte/fujisan/device.mk)
@@ -19,4 +20,10 @@ BUILD_FINGERPRINT := "ZTE/P996A26/fujisan:8.1.0/OPM1.171019.026/20190218.120220:
 TARGET_VENDOR := zte
 
 PRODUCT_SUPPORTS_BOOT_SIGNER := true
+PRODUCT_SUPPORTS_VERITY := true
+PRODUCT_SUPPORTS_VERITY_FEC := true
 PRODUCT_VERITY_SIGNING_KEY := build/make/target/product/security/verity
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
+
+PRODUCT_PACKAGES += \
+    verity_key
