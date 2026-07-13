07cf56ae286957f5b91d25ad79ac76ed029f609b
Anya1014CN <han.cf@foxmail.com>
2026-07-13 10:23:06 +0800

    fujisan: align display flags with msm8996 source

--- device.mk
+++ device.mk
@@ -34,9 +34,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    vendor.display.color@1.0-impl \
-    vendor.display.color@1.0-service
+    android.hardware.memtrack@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/fujisan.rc \
