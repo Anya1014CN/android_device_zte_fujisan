a293640cdbe948933abc84ad83a0e8b28fd62791
Anya1014CN <han.cf@foxmail.com>
2026-07-13 10:41:33 +0800

    fujisan: define msm8996 Qualcomm DRM modifiers

--- include/drm/drm_fourcc.h
+++ include/drm/drm_fourcc.h
@@ -2,7 +2,15 @@
 
 #include_next <drm/drm_fourcc.h>
 
-// Legacy fourcc_mod_code() expects the numeric vendor ID directly.
-#ifndef QCOM
-#define QCOM 0x05
+// Qualcomm modifier encoding used by the msm8996 display HAL.
+#ifndef DRM_FORMAT_MOD_QCOM_COMPRESSED
+#define DRM_FORMAT_MOD_QCOM_COMPRESSED ((0x05ULL << 56) | 0x1ULL)
+#endif
+
+#ifndef DRM_FORMAT_MOD_QCOM_DX
+#define DRM_FORMAT_MOD_QCOM_DX ((0x05ULL << 56) | 0x2ULL)
+#endif
+
+#ifndef DRM_FORMAT_MOD_QCOM_TIGHT
+#define DRM_FORMAT_MOD_QCOM_TIGHT ((0x05ULL << 56) | 0x4ULL)
 #endif
