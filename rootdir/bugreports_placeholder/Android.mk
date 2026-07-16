92b56e9717946a9855f254826293ec77cb31085d
Anya1014CN <han.cf@foxmail.com>
2026-07-16 11:01:29 +0800

    fujisan: create bugreports/bugreports dir in system image via prebuilt

--- rootdir/bugreports_placeholder/Android.mk
+++ rootdir/bugreports_placeholder/Android.mk
@@ -5,4 +5,5 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := .placeholder
 LOCAL_MODULE_PATH := $(TARGET_OUT)/bugreports/bugreports
 LOCAL_MODULE_STEM := .placeholder
+LOCAL_POST_INSTALL_CMD := rm -f $(LOCAL_INSTALLED_MODULE)
 include $(BUILD_PREBUILT)
