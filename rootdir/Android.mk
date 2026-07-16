d1cd4eb3a2595120cdce20e21908225354786f31
Anya1014CN <han.cf@foxmail.com>
2026-07-16 10:55:13 +0800

    fujisan: install vendor root placeholder to TARGET_OUT for e2fsdroid

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,7 +7,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := fujisan_legacy_vendor_root
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := .vendor-root-placeholder
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_OUT)
 LOCAL_MODULE_STEM := .fujisan_legacy_vendor_root
 LOCAL_POST_INSTALL_CMD := rm -rf $(TARGET_ROOT_OUT)/vendor $(TARGET_RECOVERY_ROOT_OUT)/vendor; ln -s /system/vendor $(TARGET_ROOT_OUT)/vendor
 include $(BUILD_PREBUILT)
