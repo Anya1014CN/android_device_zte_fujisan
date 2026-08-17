f3b2f9ae7884a6b10ec10ede4aad224aa14fcd55
Anya1014CN <han.cf@foxmail.com>
2026-08-17 13:14:24 +0800

    fujisan: label Hall sensor SELinux resources

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,6 +182,7 @@ BOARD_USES_MMCUTILS := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/system_ext/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/system_ext/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
